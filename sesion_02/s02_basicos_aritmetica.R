1ac465d109bc7b9a2e1fe084bfebaaa72148aa93
Isaac Arroyo <isaacarroyov@outlook.com>
2022-09-07 13:16:34 -0500

    [CREATE] Sesion 02 - Conociendo R y RStudio - scripts vacios

---
+++ sesion_02/s02_basicos_aritmetica.R
@@ -0,0 +1,82 @@
+# En este script se exploran algunos comandos basicos o iniciales con R 
+# asi como la aritmetica basica
+
+# COMANDOS INICIALES
+# Instalar una paqueria/libreria
+
+# Cargar la libreria en el ambiente de trabajo
+
+# Documentacion de una paqueteria o funcion
+
+# Crear variables
+
+
+# Suma 
+
+
+# Resta
+
+
+# Multiplicacion
+
+
+# elevar a la n potencia
+
+# Division 
+
+
+# Division entera
+
+# Division con residuo
+
+# Notacion cientifica
+
+
+# FUN FACT -> podemos ejecutar varias operaciones en una misma linea
+#             usando punto y coma (;)
+
+# Constantes matematicas
+
+
+# FUNCIONES ESPECIALES
+# Funciones trigonometricas
+
+# Funciones trigonometricas inversas
+
+# Funciones hipenolicas e hipervolicas inversas
+
+# Funciones especiales
+
+# Exponencial
+
+# Logaritmo natural -> ln(x)
+
+
+# Logaritmo base 10 -> log10(x)
+
+# logaritmo base n  -> log_a (x) donde a > 0'
+
+# Valor absoluto
+
+# Factorial -> !x
+
+# REDONDEO DE NUMEROS
+# round(x,n) -> redondar x a n cifras significativas 
+
+
+# ceiling(x) -> redondear a parte entera hacia arriba o redondeo hacia arriba
+
+# floor(x) -> redondear a parte entera hacia abajo o redondeo hacia abajo
+
+# trunc(x) -> parte entera de x, eliminando decimales
+
+# CREAR FUNCIONES
+# nombre de funcion <- function(argumento) {
+#                                           operaciones
+#                                           return (resultado)
+#                                           }
+
+# Elevar al cubo un elemento
+
+
+# Funcion que detecta si un numero es negativo o positivo
---
+++ sesion_02/s02_estructuras_datos.R
@@ -0,0 +1,113 @@
+# En este script vamos a explorar las estructuras de los datos que estaremos 
+# usando en la manipulacion y visualizacion de datos: vectores, factores y 
+# data frames
+
+# TIPOS DE DATOS
+
+
+# ------- VECTORES -------
+# Creacion
+
+
+# -------------------------------------------------------
+
+# Seleccionar un elemento del vector
+# seleccionar elemento 1 y 3
+
+# Determinar largo de un vector 
+
+# Selecionar ultimo elemento del vector
+
+# Modificar un elemento del vector
+# Cambiar la palabra Fulanx a tu nombre
+
+# -------------------------------------------------------
+
+# Seleccionar mas de un elemento del vector
+
+#   Seleccionar los primeros 3 y ultimos 3
+
+
+# Seleccionar elementos de los indices 1,3,6 y 9
+
+# Seleccionar elementos del 3 al 6
+
+# Seleccionar todos los indices MENOS el 4o
+
+# Seleccionar todos los elementos MENOS los indices 1,4 y 8
+
+# Modificar mas de un elemento del vector
+
+# -------------------------------------------------------
+
+# ------- MASCARAS DE VECTORES -------
+# aqui vamos a usar los operadores logicos que tiene R
+
+# Mascaras con filtros y condiciones
+# Seleccionar eleemtnos mayores a 90
+
+# Valores mayores a 50 pero menores que 250
+
+# Mascara con which
+# indices donde v6 es mayor que 90
+
+
+# Encontrar el indice del primer elmentos que sea el minimo y el maximo
+# -------------------------------------------------------
+
+# ------- SECUENCIAS DE NUMEROS -------
+
+# seq(a, b, by = d)
+
+# seq(a, b, length.out = n)
+
+# seq(a, by = d, length.out = n)
+# -------------------------------------------------------
+
+# ------- OPERACIONES CON VECTORES -------
+
+# Suma
+
+# Resta
+
+# Multiplicacion
+
+# Division
+
+# Elevar a una potencia
+
+
+# Si los vectores no son del same size
+
+
+# Funciones matematicas sobre vectores
+
+# -------------------------------------------------------
+
+# ------- FUNCIONES PROPIAS DE UN VECTOR -------
+
+# Suma de todos los elementos
+
+# Producto de todos los elementos
+
+# Maximos y minimos
+
+# Diferencia entre los elementos de un vector
+
+# Suma acumulativa
+
+# Producto acumulativo
+
+# Estadistica (media, mediana, desviacion estandar, variancia)
+
+# ------ FACTORES -> categorias -------
+
+# Crear factores de un vector 
+
+# Dar nivel a los factores
+
+
+# Dar Labels a los factores
+
+
+# Factores ordenados
