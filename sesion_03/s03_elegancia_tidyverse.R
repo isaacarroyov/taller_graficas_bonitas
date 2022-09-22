9e7d80e28f35cd99a2bce561ee003d1232c53932
Isaac Arroyo <isaacarroyov@outlook.com>
2022-09-22 01:25:53 -0500

    [UPLOAD] Sesion 03 - Script - Inicial

---
+++ sesion_03/s03_elegancia_tidyverse.R
@@ -0,0 +1,101 @@
+# ------ Sesion 03: La elegancia de Tidyverse ------
+# https://www.tidyverse.org
+
+
+# ------ CARGAR LOS BIBLIOTECAS/PAQUETERIAS ------
+# install.packages("tidyverse")
+# library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+library(lubridate)
+
+# ------ CARGAR LOS DATOS INICIALES------
+
+# ------ CONOCER EL DATAFRAME O TIBBLE ------
+# Nombre de columnas
+# Tipo de datos
+
+
+# ------ SELECCIONAR VARIABLES/COLUMNAS DE INTERES ------
+
+
+
+# ------ MUTATE ------
+# Crear una columna de precio en pesos mexicanos
+# Separar nombre y apellido de author
+
+
+# ------ MUTATE + CASE_WHEN ------
+# Crear una variable donde se vayan clasificando los precios de los libros 
+# 1 -> barato; 2 -> no tan barato;  3-> a caso vuela o que pedo?
+
+
+# ------ RENAME ------
+# Cambiar el nombres de columnas de ingles a espanol 
+# a nombres mas cortos y claros
+
+
+# ------ FILTER ------
+# Filtrar por mayo numero de semanas en el ranking (arriba de 14 semanas)
+
+
+# Por authores
+
+
+# Por palabras clave en las descripciones 
+
+# Palabra clave y ranking semanal
+
+
+
+# Usaremos los datos de moratlidad infantil
+# ------ SUMMARISE ------
+# Calcular la media y desviacion estandar de algunas columnas 
+
+
+
+# ------ GROUP_BY ------
+# Calcular la media y desviacion estandar de los diferentes continentes
+
+
+# Ver el cambio porcentual de cada continente con respecto al año pasado 
+
+
+
+# ------ LUBRIDATE ------
+
+
+# ------ MUTATE (fechas) ------
+
+
+# ------ GROUP_BY (categorias y fechas) ------
+
+
+
+# ------ PIVOT_LONGER y PIVOT_WIDER ------
+# Wider -> Longer
+
+
+# Longer -> Wider
+
+
+
+# ------ BIND_ROWS, BIND_COLS ------
+# bind_rows -> no importa que no hagan match las columnas
+
+
+
+# bind_rows -> no importa que no hagan match las columnas
+
+
+# bind_cols -> importa que el numero de filas sea el mismo
+
+
+# ------ JOINT ------
+
+
+
+
+
